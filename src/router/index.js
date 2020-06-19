3569235925cfaae5e35a433bf91658a2b9bc2cbb
wangfeng <wangfeng9036@adpanshi.com>
2020-06-19 17:04:54 +0800

    项目初始化设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+import fastClick from 'fastclick'
 Vue.use(Router)
+// 解决300毫秒问题
+fastClick.attach(document.body)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
