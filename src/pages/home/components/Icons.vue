df0cde1e46823660c4631a59b4c7d7b1fc68f0fb
wangfeng <wangfeng9036@adpanshi.com>
2020-06-28 14:38:15 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="icon-wrapper">
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon"
+                     v-for="item in page"
+                     :key="item.id"
+                >
+                     <div class="icon-img">
+                          <img class="icon-img-content" :src="item.imgUrl">
+                     </div>
+                          <p class="icon-desc">{{item.desc}}</p>
+                 </div>
+            </swiper-slide>
+        </swiper>
+       
+        
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'HomeIcons',
+    data() {
+        return {
+            iconList:[
+                {
+                    id:'0001',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门景点'
+                },
+                {
+                    id:'0002',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0003',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0004',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0005',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0006',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0007',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+                {
+                    id:'0008',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游热门旅游热门旅游热门旅游'
+                },
+                {
+                    id:'0009',
+                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                    desc:'热门旅游'
+                },
+            ]
+        }
+    },
+    computed: {
+        pages () {
+            const pages =[]
+            this.iconList.forEach((item,index) => {
+                const page = Math.floor(index / 8)
+                if (!pages[page]){
+                    pages[page] = []
+                }
+                pages[page].push(item)
+            })
+            return pages
+        }
+    }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+    @import '~style/varibles.styl'
+    @import '~style/mixins.styl'
+    .icon-wrapper
+        overflow hidden
+        height 0
+ 
+        padding-bottom 50%
+        .icon
+            position relative
+            float left
+            width 25%
+            padding-bottom 25%
+            .icon-img
+                position absolute
+                top 0
+                left 0
+                right 0
+                bottom .44rem
+                .icon-img-content
+                    height 100%
+                    display block
+                    margin 0 auto
+            .icon-desc
+                position absolute
+                left 0
+                right 0
+                bottom 0
+                height .44rem
+                line-height .44rem
+                text-align center
+                color $darkTextColor
+                ellipsis()
+</style>
